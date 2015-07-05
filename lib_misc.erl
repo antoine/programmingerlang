08e96623cfd02831368c32b831dca6db817bd4fe
antoine <antoine@delaunay.org>
2015-07-06 00:18:09 +0200

    exception handling done, on page 77

--- lib_misc.erl
+++ lib_misc.erl
@@ -1,6 +1,8 @@
 -module(lib_misc).
 -export([sum/1]).
 -export([for/3]).
+-export([perms/1]).
+-export([odds_and_event_acc/1]).
 
 sum(L) -> sum(L,0).
 
@@ -9,4 +11,18 @@ sum([H|T], N) -> sum(T, N+H).
 
 for(Max,Max,F) -> [F(Max)];
 for(I,Max,F) -> [F(I)|for(I+1,Max,F)].
+
+perms([]) -> [[]];
+perms(L) -> [[H|T] || H <- L, T <- perms(L--[H])].
+
+odds_and_event_acc(L) ->
+  odds_and_event_acc(L, [], []).
+
+odds_and_event_acc([H|T], O, E) ->
+  case H rem 2 of
+    1 -> odds_and_event_acc(T, [H|O],E);
+    0 -> odds_and_event_acc(T, O, [H|E])
+  end;
+odds_and_event_acc([], O, E) ->
+  {lists:reverse(O),lists:reverse(E)}.
 
